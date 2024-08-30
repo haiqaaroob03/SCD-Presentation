cf5751fe8b03d2cce6cafc7850ebf47c6afa7e12
Haiqa Aroob <153342409+haiqaaroob03@users.noreply.github.com>
2024-08-30 06:35:47 +0500

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleCalculator;
+
+public static class Calculate
+{
+    public static double DoCalculation(double firstNum, double secondNum, string operatorMath)
+    {
+        double result = 0;
+
+        switch (operatorMath)
+        {
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                result = firstNum / secondNum;
+                break;
+        }
+
+        return result;
+    }
+}
